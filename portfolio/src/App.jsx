f08a5cc59ce5ff073ee1ff5766c5f4e00d4ebe5f
Dejan Jovovic <dejan.jovovic2002@gmail.com>
2025-03-24 12:24:30 +0100

    init

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import NavBar from "./sections/NavBar.jsx";
+
+const App = () => {
+    return (
+        <main className="max-w-7xl mx-auto">
+            <NavBar/>
+        </main>
+    )
+}
+export default App
