8db82e0d0447693768af45ce1f3ec2a1f17ddb1e
Dejan Jovovic <dejan.jovovic2002@gmail.com>
2025-03-25 10:50:44 +0100

    imported three dependencies drei/fiber and made the canvas where the room 3d model is displayed

---
+++ portfolio/src/sections/Hero.jsx
@@ -0,0 +1,47 @@
+import {Canvas} from "@react-three/fiber";
+import {PerspectiveCamera} from "@react-three/drei";
+import {HackerRoom} from "../components/HackerRoom.jsx";
+import {Suspense} from "react";
+import CanvasLoader from "../components/CanvasLoader.jsx";
+import {useMediaQuery} from "react-responsive";
+import {calculateSizes} from "../constants/index.js";
+
+const Hero = () => {
+
+    const isSmall = useMediaQuery({maxWidth: 440});
+    const isMobile = useMediaQuery({maxWidth: 768});
+    const isTablet = useMediaQuery({minWidth: 768, maxWidth: 1024});
+
+    const sizes = calculateSizes(isSmall, isMobile, isTablet);
+
+    return (
+        <section className="min-h-screen w-full flex flex-col relative">
+            <div className="w-full mx-auto flex flex-col sm:mt-36
+            mt-20 c-space gap-3">
+                <p className="sm:text-3xl text-2xl
+                font-medium text-white text-center font-generalsans">Hi, I'm Dejan
+                    <span className="waving-hand">👋</span></p>
+                <p className="hero_tag text-gray_gradient">Building Products & Brands</p>
+            </div>
+
+            <div className="w-full h-full absolute inset-0">
+                {/*<Leva/>*/}
+                <Canvas className="w-full h-full">
+                    <Suspense fallback={<CanvasLoader/>}>
+                    {/*a camera like a human eye*/}
+                    <PerspectiveCamera makeDefault position={[0, 0, 20]}/>
+                        {/*-Math.Pi /2 is used to rotate model clockvize by 90 degrees*/}
+                    <HackerRoom
+                        position={sizes.deskPosition}
+                        rotation={[0, -Math.PI, 0]}
+                        scale={sizes.deskScale}/>
+                        <ambientLight intensity={1}/>
+                        <directionalLight position={[10, 10, 10]} intensity={0.5}/>
+                    </Suspense>
+                </Canvas>
+            </div>
+
+        </section>
+    )
+}
+export default Hero
